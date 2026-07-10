60f7e85eb2549a6920b0d3e9d6ab2cfa395eddff
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-07-11 01:43:16 +0900

    fix: cargo test cause memory explosion

--- grammar.js
+++ grammar.js
@@ -6,7 +6,7 @@ export default grammar({
   extras: ($) => [$.comment, /[ \t]/],
   externals: ($) => [$._newline, $.indent, $.dedent, $.text],
   rules: {
-    source_file: ($) => seq(repeat($._newline), repeat(choice($.heading_block, $.task_line))),
+    source_file: ($) => seq(optional($._newline), repeat(choice($.heading_block, $.task_line))),
 
     heading_block: ($) =>
       seq($.heading_line, optional(seq($.indent, $.task_block, $.dedent))),
